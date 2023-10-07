1950352ef011ec1a6a45fa3197bd9a6ede91c33c
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-10-07 12:44:37 +0700

    feat(lib): add lib functions file
    This file contains helper functions, and there are no changes from the
    previous version. We've simply added some type annotations for
    TypeScript support.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20231007054446.1204-6-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ src/lib/functions.ts
@@ -0,0 +1,95 @@
+import type { GitHubOrgsAPIResponseType, RecentMessagesReturnType } from "$types";
+import { API_URL } from "./constants";
+
+
+export const getOrgMembers = async (): Promise<GitHubOrgsAPIResponseType[]> => {
+  const response = await fetch("https://api.github.com/orgs/gnuweeb/members");
+  const data: GitHubOrgsAPIResponseType[] = await response.json();
+
+  data.sort((
+    left: GitHubOrgsAPIResponseType,
+    right: GitHubOrgsAPIResponseType
+  ) => {
+    const usernameA = left.login.toLowerCase();
+    const usernameB = right.login.toLowerCase();
+    if (usernameA < usernameB) return -1;
+    if (usernameA > usernameB) return 1;
+    return 0;
+  });
+
+  return data;
+}
+
+export const getRecentMessages = async (): Promise<RecentMessagesReturnType[] | []> => {
+  try {
+    const response = await fetch(API_URL + "/zxc.php?action=get_messages&chat_id=-1001483770714&limit=50");
+    const { data } = (await response.json()).result;
+
+    const transformedData = data.map((item: any) => {
+      return {
+        user_id: item[0],
+        username: item[1],
+        first_name: item[2],
+        last_name: item[3],
+        user_photo: item[4],
+        message_id: item[5],
+        reply_to_message_id: item[6],
+        message_type: item[7],
+        text: item[8],
+        text_entities: item[9] ? JSON.parse(item[9]) : null,
+        file: item[10],
+        date: item[11]
+      };
+    });
+
+    return transformedData.reverse();
+  } catch {
+    return [];
+  }
+}
+
+const hashCode = (name: string) => {
+  var hash = 0;
+  for (var i = 0; i < name.length; i++) {
+      var character = name.charCodeAt(i);
+      hash = ((hash << 5) - hash) + character;
+  }
+  return Math.abs(hash);
+}
+
+export const getFixedRandomColor = (name: string) => {
+  const colorData: Record<string, string> = {
+    red: "text-red-400 border-red-400",
+    yellow: "text-yellow-400 border-yellow-400",
+    blue: "text-blue-400 border-blue-400",
+    sky: "text-sky-400 border-sky-400",
+    purple: "text-purple-400 border-purple-400",
+    orange: "text-orange-400 border-orange-400",
+    amber: "text-amber-400 border-amber-400",
+    lime: "text-lime-400 border-lime-400",
+    green: "text-green-400 border-green-400",
+    emerald: "text-emerald-400 border-emerald-400",
+    teal: "text-teal-400 border-teal-400",
+    cyan: "text-cyan-400 border-cyan-400",
+    indigo: "text-indigo-400 border-indigo-400",
+    violet: "text-violet-400 border-violet-400",
+    fuchsia: "text-fuchsia-400 border-fuchsia-400",
+    pink: "text-pink-400 border-pink-400",
+    rose: "text-rose-400 border-rose-400",
+  };
+  const colorNames = Object.keys(colorData);
+  return colorData[colorNames[hashCode(name) % colorNames.length]].split(" ");
+}
+
+export const cleanMessageText = (text: string) => {
+  return text.replaceAll(/\n/g, " ");
+}
+
+export const setupUserName = (first: string, last: string | null) => {
+  const firstName = first.trimEnd();
+  let lastName = "";
+  if(last !== null) {
+    lastName = " " + last.trimEnd();
+  }
+  return [ firstName, lastName ];
+}

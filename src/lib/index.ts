a99b1a062984c5621c4e0a17ae42e01fc8aa7d35
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-10-07 12:44:38 +0700

    feat(lib): add newly created lib constants variables and functions
    This commit includes imports and exports to `index.ts`, making it easier
    to access them using `"$lib"`.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20231007054446.1204-7-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ src/lib/index.ts
@@ -0,0 +1,22 @@
+import {
+  API_URL,
+  STORAGE_URL,
+} from "./constants";
+
+import {
+  getRecentMessages,
+  getOrgMembers,
+  getFixedRandomColor,
+  cleanMessageText,
+  setupUserName
+} from "./functions";
+
+export {
+  API_URL,
+  STORAGE_URL,
+  getRecentMessages,
+  getOrgMembers,
+  getFixedRandomColor,
+  cleanMessageText,
+  setupUserName
+};

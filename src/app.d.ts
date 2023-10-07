71e115f5c48eb3913aa20d8e5b20ec6847678c63
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-10-07 12:44:35 +0700

    feat(app): add app types file
    This file is for storing the type interface of the app.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20231007054446.1204-4-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- src/app.d.ts
+++ src/app.d.ts
@@ -9,4 +9,40 @@ declare global {
 	}
 }
 
-export {};
+interface GitHubOrgsAPIResponseType {
+  login: string
+  id: number
+  node_id: string
+  avatar_url: string
+  gravatar_id: string
+  url: string
+  html_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  starred_url: string
+  subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
+  type: string
+  site_admin: boolean
+}
+
+interface RecentMessagesReturnType {
+  user_id: number
+  username: string | null
+  first_name: string
+  last_name: string | null
+  user_photo: string | null
+  message_id: number
+  reply_to_message_id: number | null
+  message_type: string
+  text: string | null
+  text_entities: string | null
+  file: string | null
+  date: string
+}
+
+export { GitHubOrgsAPIResponseType, RecentMessagesReturnType };

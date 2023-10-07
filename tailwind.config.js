d69fa0e298f3137b1773648e196617937a6ad8a0
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-10-07 12:44:33 +0700

    refactor: migrate: migrate to sveltekit framework
    This commit involves migrating the project to the SvelteKit framework,
    replacing the previous use of Vanilla JS.
    
    New files have been added to support the migration:
    - src/ (Main app source)
      - routes/ (Page routing)
        - +layout.svelte (Layout UI)
      - app.css (Main app CSS styles)
      - app.d.ts (App types)
      - app.html (Root app web page HTML)
    - .eslintignore (ESLint ignore file)
    - .eslintrc.cjs (ESLint configuration file)
    - .prettierignore (Prettier ignore file for specific files)
    - .prettierrc (Prettier configuration file for code formatting)
    - svelte.config.js (SvelteKit config file)
    - tsconfig.json (TypeScript compiler options configuration file)
    - vite.config.ts (Vite configuration file)
    
    Note that these new files were generated based on the recommendation
    from the NPM command `npm create svelte@latest`. Although it initially
    includes `routes/+page.svelte`, we've chosen not to include it for now,
    as we plan to modify `routes/+page.svelte` later in the project.
    
    Additionally, several existing files related to the project structure
    have been modified:
    - .gitignore
    - package.json
    - postcss.config.js
    - tailwind.config.js
    
    These modifications were necessary, particularly in the
    `postcss.config.js` and `tailwind.config.js` files, as they now adhere
    to the `"module"` project type and have been updated from
    `module.exports` to `export default`. Furthermore,
    the `tailwind.config.js` file has been modified to include class name
    detection for `"./src/**/*.{html,js,svelte,ts}"` files.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20231007054446.1204-2-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.html"],
+export default {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };

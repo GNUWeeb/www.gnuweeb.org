441a9eb0d25e84bf48316312d8acff98bbe644c9
Irvan Malik <irvanma@gnuweeb.org>
2025-03-03 16:10:52 +0700

    feat: Add a border to `Footer` component and use the underlying auto margin top to put it down the screen
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="pt-10 p-5 w-full">
+<footer class="mt-auto p-5 w-full border-t border-neutral-800">
   <p class="text-xs opacity-70 mx-auto text-center">
     &copy; 2023 GNU/Weeb.<br />Licensed under BSD 3-Clause License.
   </p>
